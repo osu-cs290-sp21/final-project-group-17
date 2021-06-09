4eacf7fecf98273f84831e47f830b4beb01ac0ad
Jiehui Huang <huangjie@flip2.engr.oregonstate.edu>
2021-06-08 19:25:39 -0700

    More champisons added.

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 /*
- * This function gets the Person ID from the current URL.  For example, if the
- * current URL path is  "/people/luke", this function will return "luke".
+ * 
+ * 
  */
 function getPersonIdFromURL() {
   var path = window.location.pathname;
@@ -14,16 +14,6 @@ function getPersonIdFromURL() {
   } else {
       return null;
   }
-
-  // if (pathParts[1] === "list") {
-  //   return pathParts[2];
-  // }
-  // if (pathParts[1] === "item") {
-  //   return pathParts[2];
-  // }
-  // if (pathParts[1] === "esports") {
-  //   return pathParts[2];
-  // }
 }
 
 function handleModalAcceptClick() {
@@ -71,53 +61,6 @@ function handleModalAcceptClick() {
   }
 }
 
-
-// function handleModalAcceptClick1() {
-
-//   var photoURL = document.getElementById('photo-url-input').value.trim();
-//   var caption = document.getElementById('photo-caption-input').value.trim();
-
-  
-//   if (!photoURL || !caption) {
-//     alert("You are missing either the url or caption, Please try again");
-//   } else {
-
-//     var req = new XMLHttpRequest()
-//     var reqUrl = '/item/' + getPersonIdFromURL() + '/addPhoto'
-
-//     console.log("== reqUrl:", reqUrl)
-//     req.open('POST', reqUrl)
-
-//     var photo = {
-//       url: photoURL,
-//       caption: caption
-//     }
-//     var reqBody = JSON.stringify(photo)
-//     console.log("== reqBody:", reqBody)
-//     console.log("== reqBody.url:", reqBody.url)
-//     console.log("== typeof(reqBody):", typeof(reqBody))
-
-//     req.setRequestHeader('Content-Type', 'application/json')
-
-//     req.addEventListener('load', function (event) {
-//       if (event.target.status === 200) {
-//         var photoCardTemplate = Handlebars.templates.photoCard;
-//         var newPhotoCardHTML = photoCardTemplate(photo);
-//         var photoCardContainer = document.querySelector('.photo-card-container');
-//         photoCardContainer.insertAdjacentHTML('beforeend', newPhotoCardHTML);
-//       } else {
-//         alert("Failed to add photo to database; error:\n\n" + event.target.response)
-//       }
-//     })
-   
-//     req.send(reqBody)
-
-//     hideModal();
-//    }
-// }
-
-
-
 function showModal() {
 
   var modal = document.getElementById('add-photo-modal');
@@ -160,9 +103,6 @@ window.addEventListener('DOMContentLoaded', function () {
   var modalAcceptButton = document.getElementById('modal-accept');
   modalAcceptButton.addEventListener('click', handleModalAcceptClick);
 
-  // var modalAcceptButton1 = document.getElementById('modal-accept2');
-  // modalAcceptButton1.addEventListener('click', handleModalAcceptClick1);
-
   var modalHideButtons = document.getElementsByClassName('modal-hide-button');
   for (var i = 0; i < modalHideButtons.length; i++) {
       modalHideButtons[i].addEventListener('click', hideModal);
@@ -174,7 +114,7 @@ window.addEventListener('DOMContentLoaded', function () {
 $(document).ready(function () {
   // search every keyup in the search bar.
   $('#search-bar').keyup(function () {
-    var searchInput = this.value.toLowerCase();
+    var searchInput = this.value.toLowerCase(); 
     var list = $('.person-card');
     for (var i = 0; i < list.length; ++i) {
       let id = list[i].id;
